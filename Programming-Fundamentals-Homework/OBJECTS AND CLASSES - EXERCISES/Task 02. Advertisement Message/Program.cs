9688da6bfb86554fce16b5f291f0447f3fe83383
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-10-22 12:24:51 +0300

    OBJECTS AND CLASSES - EXERCISES

---
+++ Programming-Fundamentals-Homework/OBJECTS AND CLASSES - EXERCISES/Task 02. Advertisement Message/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02.Advertisement_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phrases = new[]{
+                "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can’t live without this product." };
+
+            var events = new[] {"Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!" };
+
+            var authors = new[] {
+                "Diana",
+                "Petya",
+                "Stella",
+                "Elena",
+                "Katya",
+                "Iva",
+                "Annie",
+                "Eva" };
+
+            var cities = new[] {
+                "Burgas",
+                "Sofia",
+                "Plovdiv",
+                "Varna",
+                "Ruse" };
+
+            Random phrasesIndex = new Random();
+            var phrRandom = phrasesIndex.Next(0, phrases.Length);
+            var phrasesOfRandomElement = phrases[phrRandom];
+
+            Random eventsIndex = new Random();
+            var eventsRandom = eventsIndex.Next(0, events.Length);
+            var elemntOfRandomElement = events[eventsRandom];
+
+            Random authorsIndex = new Random();
+            var authorsRandom = authorsIndex.Next(0, authors.Length);
+            var authorsOfRandomElement = authors[authorsRandom];
+
+            Random citiesIndex = new Random();
+            var citiesRandom = citiesIndex.Next(0, cities.Length);
+            var citiesOfRandomElement = cities[citiesRandom];
+
+            Console.WriteLine($"{phrasesOfRandomElement} {elemntOfRandomElement} {authorsOfRandomElement} – {citiesOfRandomElement}.");
+
+
+
+
+
+        }
+    }
+}
+
---
+++ Programming-Fundamentals-Homework/OBJECTS AND CLASSES - EXERCISES/Task 05. Book Library/Program.cs
@@ -0,0 +1,78 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Globalization;
+
+    namespace P05_BookLibrary
+    {
+        class P05_BookLibrary
+        {
+            static void Main(string[] args)
+            {
+                var booksCount = int.Parse(Console.ReadLine());
+                Library libraryOfBooks = GetLibraryOfBooks(booksCount);
+
+                var authorsBooksTotalPrice = GetTotalBooksPriveByAuthor(libraryOfBooks);
+
+                PrintTotalBooksPriveByAuthor(authorsBooksTotalPrice);
+            }
+
+            static void PrintTotalBooksPriveByAuthor(Dictionary<string, double> authorsBooksTotalPrice)
+            {
+                foreach (var author in authorsBooksTotalPrice.OrderByDescending(p => p.Value).ThenBy(a => a.Key))
+                {
+                    Console.WriteLine($"{author.Key} -> {author.Value:f2}");
+                }
+            }
+
+            private static Dictionary<string, double> GetTotalBooksPriveByAuthor(Library libraryOfBooks)
+            {
+                var totalBooksPriveByAuthor = new Dictionary<string, double>();
+                foreach (var book in libraryOfBooks.Books)
+                {
+                    if (!totalBooksPriveByAuthor.ContainsKey(book.Author))
+                    {
+                        totalBooksPriveByAuthor[book.Author] = 0;
+                    }
+                    totalBooksPriveByAuthor[book.Author] += book.Price;
+                }
+
+                return totalBooksPriveByAuthor;
+            }
+
+            static Library GetLibraryOfBooks(int booksCount)
+            {
+                Library libratyOfBooks = new Library();
+                var booksList = new List<Book>();
+                for (int i = 0; i < booksCount; i++)
+                {
+                    libratyOfBooks.Books.Add(GetBook());
+                }
+
+                return libratyOfBooks;
+            }
+
+            private static Book GetBook()
+            {          
+                var bookInfo = Console.ReadLine()
+                    .Split(' ')
+                    .ToArray();
+                return new Book
+                {
+                    Author = bookInfo.Skip(1).First(),              
+                    Price = double.Parse(bookInfo.Skip(5).First())
+                };
+            }
+        }
+
+        class Library
+        {      
+            public List<Book> Books = new List<Book>();
+        }
+
+        class Book
+        {      
+            public string Author { get; set; }      
+            public double Price { get; set; }
+        }
+    }
