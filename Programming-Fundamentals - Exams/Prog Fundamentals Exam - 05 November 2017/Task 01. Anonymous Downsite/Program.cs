fea9b51c33113ef2f2255c233d6032ad9fe4191c
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-10 18:37:58 +0200

    Adding exam from 05.11.17

---
+++ Programming-Fundamentals - Exams/Prog Fundamentals Exam - 05 November 2017/Task 01. Anonymous Downsite/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01.Anonymous_Downsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double securityKey = double.Parse(Console.ReadLine());
+            decimal totalLoss = 0;
+            double securityToken = 0;
+          
+            List<string> websites = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                var data = Console.ReadLine().Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries);
+                //{siteName} {siteVisits} {siteCommercialPricePerVisit}              
+                var siteName = data[0];
+                long siteVisits = long.Parse(data[1]);
+                decimal siteCommercialPricePerVisit = decimal.Parse(data[2]);
+                //formula: siteVisits * siteCommercialPricePerVisit
+                totalLoss += siteVisits * siteCommercialPricePerVisit;
+                securityToken = Math.Pow(securityKey, n);        
+                websites.Add(siteName);
+                
+            }
+            foreach (var web in websites)
+            {
+                Console.WriteLine(web);
+            }
+            Console.WriteLine($"Total Loss: {totalLoss:F20}");
+            Console.WriteLine($"Security Token: {securityToken}");
+        }
+    }
+}
---
+++ Programming-Fundamentals - Exams/Prog Fundamentals Exam - 05 November 2017/Task 03. Anonymous Vox/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_03.Anonymous_Vox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            var placeholders = Console.ReadLine().Split(new char[] {'{', '}'}, StringSplitOptions.RemoveEmptyEntries);
+            string pattern = @"([A-Za-z]+)(.+)\1";
+
+            Regex reg = new Regex(pattern);
+            MatchCollection matches = reg.Matches(text);
+            int placeholderIndex = 0;
+            foreach (Match match in matches)
+            {
+                if (placeholderIndex >= placeholders.Length)
+                {
+                    break;
+                }
+                else
+                {
+                    text = ReplaceFirst(text, match.Groups[2].Value, placeholders[placeholderIndex++]);
+                }
+            }
+            Console.WriteLine(text);
+        }
+
+        static string ReplaceFirst(string text, string oldValue, string newValue)
+        {
+            string substringWithOldValue = text.Substring(0, text.IndexOf(oldValue) + oldValue.Length);
+
+            string substringWithNewValue = substringWithOldValue.Replace(oldValue, newValue);
+
+            return substringWithNewValue + text.Substring(substringWithOldValue.Length);         
+        }
+    }
+}
