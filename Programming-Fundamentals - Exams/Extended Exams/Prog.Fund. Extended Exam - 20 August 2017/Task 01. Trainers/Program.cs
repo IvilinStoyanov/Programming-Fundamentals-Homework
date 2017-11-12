2b31b675f8b17c654777c7750fe56fbeab857cb9
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-12 21:49:24 +0200

    adding first task.

---
+++ Programming-Fundamentals - Exams/Extended Exams/Prog.Fund. Extended Exam - 20 August 2017/Task 01. Trainers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()                                  // 90/100 Cuz of the team names.
+    {
+        int count = int.Parse(Console.ReadLine());
+        // store variables data
+       
+        var result = new SortedDictionary<string, decimal>();
+        
+        for (int i = 0; i < count; i++)
+        {
+            // input data
+            long distanceInMeters = long.Parse(Console.ReadLine()) * 1600;
+            decimal cargoInKilograms = decimal.Parse(Console.ReadLine()) * 1000;
+            string teamName = Console.ReadLine();
+            // converting data
+            decimal cargoIncome = 1.5m * cargoInKilograms;
+            decimal fuelConsumption = 0.7m * distanceInMeters * 2.5m;
+
+            decimal participantEarnedMoney = cargoIncome - fuelConsumption;
+            if (!result.ContainsKey(teamName))
+            {
+                result.Add(teamName,new decimal());                       
+            }
+            if (result.ContainsKey(teamName))
+            {
+                result[teamName] += participantEarnedMoney;
+            }
+
+        }
+        var winner = result.OrderByDescending(x => x.Value).FirstOrDefault();
+       
+        Console.WriteLine($"The {winner.Key} Trainers win with ${winner.Value:F3}.");
+    }
+}
+
