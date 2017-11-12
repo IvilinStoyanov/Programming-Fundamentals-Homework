ed5d123e158a74092f57811f0b7014dc21e26598
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-12 15:02:58 +0200

    Adding extended exam from 04 Sept.

---
+++ Programming-Fundamentals - Exams/Extended Exams/Progr. Fund. Extended - Exam 04 Sept/Task 02. Icarus/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02.Icarus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int startingPositionOfIndex = int.Parse(Console.ReadLine());
+
+            var commands = Console.ReadLine();
+            var initialDamage = 1;
+            while (commands != "Supernova")
+            {
+                var commandsArgs = commands.Split(' ');
+                var direction = commandsArgs[0];
+                var steps = int.Parse(commandsArgs[1]);
+                if (direction == "left")
+                {                                                
+                        while (steps > 0)
+                        {
+                            startingPositionOfIndex--;
+                            if (startingPositionOfIndex < 0)
+                            {
+                                startingPositionOfIndex = planes.Length - 1;
+                                initialDamage++;
+                            }
+                            planes[startingPositionOfIndex] -= initialDamage;
+                            steps--;
+                        }
+                    }               
+                if (direction == "right")
+                {
+                  while (steps > 0)
+                    {
+                        startingPositionOfIndex++;
+                        if(startingPositionOfIndex > planes.Length - 1)
+                        {
+                            startingPositionOfIndex = 0;
+                            initialDamage++;
+                        }
+                        planes[startingPositionOfIndex] -= initialDamage;
+                        steps--;
+                    }                    
+                }
+                commands = Console.ReadLine(); // read input again
+            }
+            Console.WriteLine(string.Join(" ", planes));          
+        }
+    }
+}
