998d88679507836ab80125a726037c5eb840fab7
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-13 21:21:09 +0200

    Adding Task 03. Trainegram

---
+++ Programming-Fundamentals - Exams/Extended Exams/Prog.Fund. Extended Exam - 20 August 2017/Task 03. Trainegram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_03.Trainegram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^(\<\[[^A-Za-z0-9]*\][\.]{1})+([\.]{1}\[[A-Za-z0-9]*\][\.]{1})*$";
+
+            string data = Console.ReadLine();
+            
+            while(data != "Traincode!")
+            {
+                Match Istrain = Regex.Match(data, pattern);
+                if (Istrain.Success)
+                {
+                    Console.WriteLine(data);
+                }
+                data = Console.ReadLine();
+            }
+        }
+    }
+}
