0136efc9a2be4da63f2da039d89d6c04411a70db
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-14 20:49:06 +0200

    Adding Task 01. Splinter Trip

---
+++ Programming-Fundamentals - Exams/Extended Exams/Prog. Fund. Extended - Exam  09 May 2017/Task 01. Splinter Trip/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        /* info : 
+     * consumes a lot of fuel - 25L per mile 
+     *  Heavy winds need 1.5 times more fuel.
+     *  the total fuel amount we put in needs to increase by 5%.
+        */
+        double tripDistanceInMiles = double.Parse(Console.ReadLine());
+        double fuelTankCapacityInLiters = double.Parse(Console.ReadLine());
+        double milesSpentInHeavyWinds = double.Parse(Console.ReadLine());
+
+        double NonHeavyWindsConsumption = (tripDistanceInMiles - milesSpentInHeavyWinds) * 25;
+        double HeavyWindsConsumption = milesSpentInHeavyWinds * (25 * 1.5);
+        double FuelConsumption = NonHeavyWindsConsumption + HeavyWindsConsumption;
+        double tolerance = FuelConsumption * 1.05;
+        double remainingFuel = fuelTankCapacityInLiters - tolerance;
+        double result = Math.Abs(remainingFuel);
+        Console.WriteLine($"Fuel needed: {tolerance:F2}L");
+        if(remainingFuel >= 0)
+        {
+            Console.WriteLine($"Enough with {result:F2}L to spare!");
+        }
+        else
+        {
+            Console.WriteLine($"We need {result:F2}L more fuel.");
+        }
+    }
+}
+
