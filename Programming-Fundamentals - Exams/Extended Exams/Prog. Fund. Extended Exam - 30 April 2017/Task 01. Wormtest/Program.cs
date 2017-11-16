c612c836ad8d8754f51c85fd3c2da8c12d90f290
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-16 20:05:17 +0200

    Adding Task 01. Wormtest and Task 02. WormIpsum

---
+++ Programming-Fundamentals - Exams/Extended Exams/Prog. Fund. Extended Exam - 30 April 2017/Task 01. Wormtest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01.Wormtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lengthCentimeters = double.Parse(Console.ReadLine()) * 100;
+            double widthCentimeters = double.Parse(Console.ReadLine());
+           
+            if(lengthCentimeters % widthCentimeters == 0 || widthCentimeters == 0)
+            {
+                Console.WriteLine("{0:F2}" , lengthCentimeters * widthCentimeters);
+            }
+            else
+            {
+                double result = (lengthCentimeters / widthCentimeters) * 100;
+                Console.WriteLine("{0:F2}%" , result);
+            }
+        }
+    }
+}
---
+++ Programming-Fundamentals - Exams/Extended Exams/Prog. Fund. Extended Exam - 30 April 2017/Task 02. Worm Ipsum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        var sentenceReg = new Regex(@"^[A-Z][^\.]*\.$");
+        var wordsReg = new Regex(@"[^\s,\.]+");
+
+        var input = Console.ReadLine();
+        while (!input.Equals("Worm Ipsum"))
+        {
+            var sentaceMatch = sentenceReg.Match(input);
+            if(sentaceMatch.Success)
+            {
+                var wordMatches = wordsReg.Matches(input);
+                foreach (Match word in wordMatches)
+                {
+                    var currentWord = word.Value;
+                    if(currentWord.Length != currentWord.Distinct().Count())
+                    {
+                        var symbol = currentWord.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+                        var newWord = string.Concat(Enumerable.Repeat(symbol, currentWord.Length));
+                        input = Regex.Replace(input, currentWord, newWord);
+                    }
+                }
+                Console.WriteLine(input);
+            }
+            input = Console.ReadLine();
+        }
+    }
+}
+
