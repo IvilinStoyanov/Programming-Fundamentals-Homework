d64f1730d817bd9fbf9641b374eda0742b5f9367
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-16 17:18:36 +0200

    Adding Task 04. NSA

---
+++ Programming-Fundamentals - Exams/Extended Exams/Prog. Fund. Extended - Exam  09 May 2017/Task 04. NSA/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_04.NSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spiesData = new Dictionary<string, Dictionary<string, int>>();
+            var data = Console.ReadLine();
+            while(data != "quit")
+            {
+                var dataSplit = data.Split(new char[] { ' ', '>', '-' }, StringSplitOptions.RemoveEmptyEntries);
+                var countryName = dataSplit[0];
+                var spiesName = dataSplit[1];
+                var daysInService = int.Parse(dataSplit[2]);
+
+                if(!spiesData.ContainsKey(countryName))
+                {
+                    spiesData.Add(countryName, new Dictionary<string, int>());
+                }
+                if(!spiesData[countryName].ContainsKey(spiesName))
+                {
+                    spiesData[countryName].Add(spiesName, new int());
+                    spiesData[countryName][spiesName] = daysInService;
+                }
+                if (spiesData[countryName].ContainsKey(spiesName))
+                {                   
+                    spiesData[countryName][spiesName] = daysInService;
+                }
+                data = Console.ReadLine();
+            }
+            foreach (var country in spiesData.OrderByDescending(s => s.Value.Count))
+            {
+                Console.WriteLine("Country: " + country.Key);
+                foreach (var spies in country.Value.OrderByDescending(s => s.Value))
+                {
+                    Console.WriteLine($"**{spies.Key} : {spies.Value}");                  
+                }
+            }
+        }
+    }
+}
