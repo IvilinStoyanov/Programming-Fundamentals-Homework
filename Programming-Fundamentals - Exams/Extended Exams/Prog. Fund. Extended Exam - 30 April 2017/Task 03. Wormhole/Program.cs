0c263019c1f815c63869b836ecce0f979b70fbdf
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-17 13:02:27 +0200

    Adding Task 03. Wormhole

---
+++ Programming-Fundamentals - Exams/Extended Exams/Prog. Fund. Extended Exam - 30 April 2017/Task 03. Wormhole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        var wormholes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int counter = 0;
+        int currentWormhole = 0;
+        for (int i = 0; i < wormholes.Length; i++)
+        {
+            if(wormholes[i] == 0)
+            {
+                counter++;
+            }
+            if(wormholes[i] != 0)
+            {
+                currentWormhole = wormholes[i];
+                wormholes[i] = 0;
+                i = currentWormhole;
+                counter++;
+            }
+        }
+        Console.WriteLine(counter);
+    }
+}
