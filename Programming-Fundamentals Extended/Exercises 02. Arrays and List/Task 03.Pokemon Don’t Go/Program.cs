99f25a9ac7eb839267ead345fe94aef42060285b
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-20 20:40:12 +0200

    Adding Task 03. Pokemon Don't Go

---
+++ Programming-Fundamentals Extended/Exercises 02. Arrays and List/Task 03.Pokemon Don’t Go/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        var pokemons = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
+        int removedPokomons = 0;
+      
+        int element;
+        while(pokemons.Count > 0)
+        {
+            int indexValue = int.Parse(Console.ReadLine());
+            if (indexValue < 0)
+            {
+                element = pokemons[0];
+                pokemons[0] = pokemons[pokemons.Count - 1];
+            }
+            else if (indexValue >= pokemons.Count)
+            {
+                element = pokemons[pokemons.Count - 1];
+                pokemons[pokemons.Count - 1] = pokemons[0];
+            }
+            else
+            {
+                element = pokemons[indexValue];
+                pokemons.RemoveAt(indexValue);
+            }
+            removedPokomons += element;
+
+
+            for (int i = 0; i < pokemons.Count; i++)
+            {
+                if(element >= pokemons[i])
+                {
+                    pokemons[i] += element;
+                }
+                else
+                {
+                    pokemons[i] -= element;
+                }             
+            }           
+        }
+        Console.WriteLine(removedPokomons);
+    }
+}
