5b9717a0e32976cbc294fc47b99b9abdb833d18e
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-25 15:39:39 +0200

    Adding Task 01.Trainegram from Excercises 03.Strings and Regex

---
+++ Programming-Fundamentals Extended/Exercises 03. Strings and Regex - Exercise/Task 01.Trainegram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_01.Trainegram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^(\<\[[^A-Za-z0-9]*\]\.{1})+(\.{1}\[[A-Za-z0-9]*\]\.{1})*$";
+            List<string> validTrains = new List<string>();
+
+            string input = Console.ReadLine();
+            while (!input.Equals("Traincode!"))
+            {             
+                if(Regex.IsMatch(input , pattern))
+                {
+                    validTrains.Add(input);
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var trains in validTrains)
+            {
+                Console.WriteLine(trains);
+            }
+        }
+    }
+}
