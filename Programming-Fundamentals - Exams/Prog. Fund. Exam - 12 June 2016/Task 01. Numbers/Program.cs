6e775264844aa1655a3ab068c7fcaf7fa11e4826
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-11-29 11:57:36 +0200

    Adding Task 01. Numbers

---
+++ Programming-Fundamentals - Exams/Prog. Fund. Exam - 12 June 2016/Task 01. Numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01.Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input 
+            var numbers = Console.ReadLine().Split(' ').Select(long.Parse).OrderByDescending(x => x).ToArray();
+            var avegareOfNumbers = numbers.Average();
+            
+            List<long> filteredNumbers = new List<long>();
+            if (numbers.Length == 1 || numbers.Length == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            if(avegareOfNumbers == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > avegareOfNumbers && numbers.Length != 0)
+                {
+                    filteredNumbers.Add(numbers[i]);
+                }
+            }
+            if (filteredNumbers.Count < 5)
+            {
+                for (int i = 0; i < filteredNumbers.Count; i++)
+                {
+                    Console.Write(filteredNumbers[i] + " ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write(filteredNumbers[i] + " ");
+                }
+            }                 
+        }
+    }
+}
