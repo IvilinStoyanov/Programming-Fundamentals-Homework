165bdf0bc913165b626499c8181093289a0732d9
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2017-12-04 20:31:07 +0200

    Adding Task 04. Trainlands

---
+++ Programming-Fundamentals Extended/Exercises 04. Dictionaries and LINQ - Exercises/Task 03.Phoenix Oscar/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03.Phoenix_Oscar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Dictionary<string, HashSet<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Blaze it!")
+            {
+                string[] myArr = input
+                    .Split(new string[] { " -> " }, StringSplitOptions.None);
+
+                string creature = myArr[0];
+                string squadMate = myArr[1];
+
+                if (!data.ContainsKey(creature))
+                {
+                    data.Add(creature, new HashSet<string>());
+                }
+
+                if (creature != squadMate)
+                {
+                    data[creature].Add(squadMate);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var result = new Dictionary<string, List<string>>();
+
+            foreach (var item in data)
+            {
+                result.Add(item.Key, new List<string>());
+
+                foreach (var mate in item.Value)
+                {
+                    if (data.ContainsKey(mate) && data[mate].Contains(item.Key))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        result[item.Key].Add(mate);
+                    }
+                }
+            }
+            foreach (var item in result.OrderByDescending(c => c.Value.Count))
+            {
+                Console.WriteLine($"{item.Key} : {item.Value.Count}");
+            }
+        }      
+    }
+}
---
+++ Programming-Fundamentals Extended/Exercises 04. Dictionaries and LINQ - Exercises/Task 04. Trainlands/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        // {trainName} -> {wagonName} : {wagonPower}
+        Dictionary<string, Dictionary<string, int>> trains = new Dictionary<string, Dictionary<string, int>>();
+        var data = Console.ReadLine();
+        while (data != "It's Training Men!")
+        {
+            var dataSplit = data.Split(new string[] { " -> ", " : ", " = " }, StringSplitOptions.RemoveEmptyEntries);
+            // data after split 
+            string trainName = dataSplit[0];
+            // adding data to dictionary
+            if (dataSplit.Length >= 3)
+            {
+                string wagonName = dataSplit[1];
+                int wagonPower = int.Parse(dataSplit[2]);
+
+                if (!trains.ContainsKey(trainName))
+                {
+                    trains.Add(trainName, new Dictionary<string, int>());
+                }
+                trains[trainName].Add(wagonName, wagonPower);
+            }
+            else
+            {
+                string otherTrainName = dataSplit[1];
+                if (data.Contains(" = "))
+                {
+                    if (!trains.ContainsKey(trainName))
+                    {
+                        trains[trainName] = new Dictionary<string, int>();
+                    }
+                    trains[trainName].Clear();
+                    foreach (var wagon in trains[otherTrainName])
+                    {
+                        trains[trainName].Add(wagon.Key, wagon.Value);
+                    }
+                }
+                else
+                {
+                    if (!trains.ContainsKey(trainName))
+                    {
+                        trains[trainName] = new Dictionary<string, int>();
+                    }
+                    foreach (var wagons in trains[otherTrainName])
+                    {
+                        trains[trainName].Add(wagons.Key, wagons.Value);
+                    }
+                    trains.Remove(otherTrainName);
+                }
+
+            }
+            data = Console.ReadLine();
+        }
+        foreach (var train in trains
+               .OrderByDescending(t => t.Value.Values.Sum())
+               .ThenBy(t => t.Value.Count()))
+        {
+            Console.WriteLine($"Train: {train.Key}");
+
+            foreach (var wagon in trains[train.Key]
+                .OrderByDescending(w => w.Value))
+            {
+                Console.WriteLine($"###{wagon.Key} - {wagon.Value}");
+            }
+        }
+    }
+}
+
